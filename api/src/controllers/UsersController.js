557c030acfe657651a655c8660449bc3e4d5885c
lucasblk <lukaslopz@gmail.com>
2024-08-07 20:25:18 -0300

    post

---
+++ api/src/controllers/UsersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+    /**
+     * index - GET para listar varios registros.
+     * show - GET para exibir um registro especifico.
+     * create - POST para criar um novo registro.
+     * update - PUT para atualizar o registro.
+     * delete - DELETE para remover um registro.
+     */
+}
+
+module.exports = UsersController;
